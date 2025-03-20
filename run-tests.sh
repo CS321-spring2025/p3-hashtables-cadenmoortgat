f30ed83f387837e3ab27506040ac9c7e26ca664c
smyli <cadenmoortgat@u.boisestate.edu>
2025-03-19 23:18:56 -0500

    Added in all the tests

--- run-tests.sh
+++ run-tests.sh
@@ -2,7 +2,7 @@
 
 function header(){
 	output=$1
-	for i in {1..80} 
+	for i in {1..80}
 	do
 		echo -n "-" >> $output
 	done
@@ -24,38 +24,49 @@ fi
 
 echo
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "Running test for word-list for varying load factors"
+echo "Running tests for word-list for varying load factors"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
 
-
+# Convert test-cases line endings if needed
 dos2unix test-cases/* >& /dev/null
+
+# We only have reference dump files for data source = 3 (word-list)
 debugLevel=1
-load=0.5
 
-echo "Running java HashtableExperiment dataSource = 3 loadFactor = $load "
-java HashtableExperiment 3 $load $debugLevel  >> /dev/null
-dos2unix linear-dump.txt double-dump.txt  >& /dev/null
+# The set of load factors to test
+for load in 0.5 0.6 0.7 0.8 0.9 0.95 0.99
+do
+    echo "Running java HashtableExperiment dataSource = 3 loadFactor = $load with debugLevel=$debugLevel"
+    java HashtableExperiment 3 $load $debugLevel >> /dev/null
 
-echo
-diff -w -B  linear-dump.txt test-cases/word-list-$load-linear-dump.txt > diff-linear-$load.out
-if test "$?" = 0
-then
-	echo "Test PASSED for linear probing and load = $load"
-	/bin/rm -f diff-linear-$load.out
-else
-	echo "==> Test FAILED for linear probing load = $load!! "
-	echo "       Check the file diff-linear-$load.out for differences"
-fi
+    # Convert newly created dump files (in case line endings differ)
+    dos2unix linear-dump.txt double-dump.txt >& /dev/null
 
-diff -w -B  double-dump.txt test-cases/word-list-$load-double-dump.txt > diff-double-$load.out
-if test "$?" = 0
-then
-	echo "Test PASSED for double probing and load = $load"
-	/bin/rm -f diff-double-$load.out
-else
-	echo "==> Test FAILED for double probing load = $load!! "
-	echo "       Check the file diff-double-$load.out for differences"
-fi
-echo
+    echo
+    # Compare linear-dump.txt
+    diff -w -B linear-dump.txt test-cases/word-list-$load-linear-dump.txt > diff-linear-$load.out
+    if test "$?" = 0
+    then
+        echo "Test PASSED for linear probing (dataSource=3, load=$load)"
+        /bin/rm -f diff-linear-$load.out
+    else
+        echo "==> Test FAILED for linear probing (dataSource=3, load=$load)!!"
+        echo "       Check the file diff-linear-$load.out for differences"
+    fi
 
+    # Compare double-dump.txt
+    diff -w -B double-dump.txt test-cases/word-list-$load-double-dump.txt > diff-double-$load.out
+    if test "$?" = 0
+    then
+        echo "Test PASSED for double hashing (dataSource=3, load=$load)"
+        /bin/rm -f diff-double-$load.out
+    else
+        echo "==> Test FAILED for double hashing (dataSource=3, load=$load)!!"
+        echo "       Check the file diff-double-$load.out for differences"
+    fi
+
+    echo
+done
+
+echo "All tests completed."
